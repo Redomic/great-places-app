810ec116f161e5bfd25dd3835a410764a6d60c09
Redomic <redomicmc@gmail.com>
2021-06-28 14:09:32 +0530

    add location input

---
+++ lib/widgets/location_input.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class LocationInput extends StatefulWidget {
+
+  @override
+  _LocationInputState createState() => _LocationInputState();
+}
+
+class _LocationInputState extends State<LocationInput> {
+  String _previewImageUrl = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          height: 170,
+          width: double.infinity,
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            border: Border.all(width: 1, color: Colors.grey)
+          ),
+          child: _previewImageUrl == ''
+              ? Text(
+                  'No Location chosen',
+                  textAlign: TextAlign.center,
+                )
+              : Image.network(
+                  _previewImageUrl,
+                  fit: BoxFit.cover,
+                  width: double.infinity,
+                ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextButton.icon(
+              icon: Icon(
+                Icons.location_on,
+              ),
+              label: Text('Current Location'),
+              onPressed: () {},
+            ),
+            TextButton.icon(
+              icon: Icon(
+                Icons.map,
+              ),
+              label: Text('Select on Map'),
+              onPressed: () {},
+            )
+          ],
+        ),
+      ],
+    );
+  }
+}
