699d7e621306ba0bbdf7ee9bfe932d746f124ec1
Redomic <redomicmc@gmail.com>
2021-06-28 14:13:05 +0530

    add current location function

--- lib/widgets/location_input.dart
+++ lib/widgets/location_input.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:location/location.dart';
 
 class LocationInput extends StatefulWidget {
 
@@ -8,6 +9,12 @@ class LocationInput extends StatefulWidget {
 
 class _LocationInputState extends State<LocationInput> {
   String _previewImageUrl = '';
+
+  Future<void> _getCurrentLocation() async {
+    final locData = await Location().getLocation();
+    print(locData.latitude);
+    print(locData.longitude);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -39,7 +46,7 @@ class _LocationInputState extends State<LocationInput> {
                 Icons.location_on,
               ),
               label: Text('Current Location'),
-              onPressed: () {},
+              onPressed: _getCurrentLocation,
             ),
             TextButton.icon(
               icon: Icon(
